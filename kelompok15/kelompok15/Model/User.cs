ea9ba3e98cc9a2ec80c8b7a12e26153fc7d31a2c
attaufiqi <attaufiqi@users.noreply.github.com>
2018-10-29 23:26:14 +0700

    modul1kelompok15

---
+++ kelompok15/kelompok15/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kelompok15.Model
+{
+    class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ kelompok15/kelompok15/Model/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kelompok15.Model
+{
+    class UserLogin
+    {
+        public static string Username = "kelompok15";
+        public static string Password = "kelompok15";
+    }
+}
